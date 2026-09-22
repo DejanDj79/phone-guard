3d9fc409987806ea52df46b559755ba71d715d86
DejanDj79 <deki4679@gmail.com>
2026-09-22 13:18:22 +0200

    Add parent main activity

---
+++ parent/src/main/java/com/example/phoneguard/parent/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.phoneguard.parent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.phoneguard.parent.ui.ParentDashboardScreen
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
+    setContent {
+      MaterialTheme {
+        Surface {
+          ParentDashboardScreen()
+        }
+      }
+    }
+  }
+}
