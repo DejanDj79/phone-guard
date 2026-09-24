f2ae845fdb25366730a73a2a197bd91ceb7343ef
DejanDj79 <deki4679@gmail.com>
2026-09-24 13:57:47 +0200

    Add lifecycle support for Parent security gate

--- parent/build.gradle.kts
+++ parent/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.activity.compose)
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.tooling.preview)
