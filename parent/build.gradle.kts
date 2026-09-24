673cc8ae5bfb86c766a123c01ea9d7d7dafad5e8
DejanDj79 <deki4679@gmail.com>
2026-09-24 14:15:50 +0200

    Enable biometric authentication in Parent app

--- parent/build.gradle.kts
+++ parent/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.biometric)
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.androidx.compose.material3)
