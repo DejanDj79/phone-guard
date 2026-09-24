137f5fdc1679a67058ba91792bf744c26d00d20a
DejanDj79 <deki4679@gmail.com>
2026-09-25 00:38:16 +0200

    Remove obsolete offline heartbeat tracking

--- supabase/functions/heartbeat/index.ts
+++ supabase/functions/heartbeat/index.ts
@@ -162,7 +162,6 @@ export default {
         battery_unrestricted: batteryUnrestricted,
         protection_updated_at: now,
         last_seen_at: now,
-        offline_alert_sent_at: null,
         updated_at: now,
         ...(hasUsagePayload
           ? {
